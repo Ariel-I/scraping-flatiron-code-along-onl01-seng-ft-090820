44ff829ab6c1f3cc7ac23cfb569d218ad5f3f7e8
Ariel Ibarra <arielibarra39@gmail.com>
2020-09-29 23:46:41 +0000

    Automatically backed up by Learn

--- lib/my_scraper.rb
+++ lib/my_scraper.rb
@@ -10,6 +10,13 @@ class Scraper
     doc = Nokogiri::HTML(open("https://www.solabeeflowers.com/collections/plants"))
     binding.pry 
     
+     # doc.css(".card").each do |post|
+     # plant = Course.new
+     # course.title = post.css("h2").text
+     # course.schedule = post.css(".date").text
+    # course.description = post.css("p").text
+   # end
+    
   end 
   
   end
