df6a5751911644c51296aec17c413bcabcc47587
Ariel Ibarra <arielibarra39@gmail.com>
2020-09-29 23:39:10 +0000

    Automatically backed up by Learn

---
+++ lib/my_scraper.rb
@@ -0,0 +1,13 @@
+require 'nokogiri'
+require 'open-uri'
+require 'pry'
+
+require_relative './my_plant.rb'
+
+class Scraper
+  
+  def get_page
+    doc = Nokogiri::HTML(open("https://www.solabeeflowers.com/collections/plants"))
+    binding.pry 
+    
+  end 
